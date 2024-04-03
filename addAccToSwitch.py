099b5b912ad17b872407ccc0050e9abce1b363e2
MrPropreuuh <vincent.fougere@epsi.fr>
2024-04-03 23:44:23 +0200

    remove timer to add account faster

--- addAccToSwitch.py
+++ addAccToSwitch.py
@@ -1,3 +1,4 @@
+import time
 import pyautogui
 import json
 import os
@@ -31,6 +32,22 @@ def cliquer_sur_image(nom_image, attente_apres=0.3, attendre_confirmation=False)
 # Fonction pour attendre la confirmation avant de continuer
 
 
+def trouver_image(nom_image, timeout=30):
+    chemin_image = os.path.join(os.getcwd(), 'images', nom_image)
+    debut = time.time()
+    trouve = False
+    while (time.time() - debut) < timeout:
+        position = pyautogui.locateCenterOnScreen(chemin_image, confidence=0.8)
+        if position:
+            print(f"Image {nom_image} trouvée.")
+            trouve = True
+            break
+        sleep(0.2)  # Attendre un peu avant de réessayer
+    if not trouve:
+        print(f"Image {nom_image} non trouvée après {timeout} secondes.")
+    return trouve
+
+
 def attendre_confirmation(nom_image):
     print("En attente de la confirmation...")
     cliquer_sur_image(nom_image, attente_apres=0.3, attendre_confirmation=True)
@@ -44,19 +61,18 @@ def main():
     for compte in comptes:
         # Cliquer sur "ajout_account_auto.png"
         cliquer_sur_image("ajout_account_auto.png")
-
+        sleep(0.1)
         # Cliquer sur "blank.png" pour sélectionner le champ du pseudo
         cliquer_sur_image("blank.png")
 
         # Écrire le nom d'utilisateur
         pyautogui.write(compte["username"])
-        sleep(0.1)
 
         # Cliquer sur "add_offline.png" pour ajouter le compte
         cliquer_sur_image("add_offline.png")
 
         # Attendre la confirmation avant de passer au prochain compte
-        attendre_confirmation("confirm_button.png")
+        trouver_image("confirm_button.png")
 
 
 if __name__ == "__main__":
